b2ffe7e373c382cda15b4559eaf8325bbf8f426c
Mun, Younghwan <younghwan.mun@teradata.com>
2020-06-07 19:59:29 -0400

    Add customizing rest api framework ui

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cleanup.apps.CleanupConfig',    
+    'django_cleanup.apps.CleanupConfig',
     'main',
     'projects',
     'rest_framework',
