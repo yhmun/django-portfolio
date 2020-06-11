a2fa17ca1989b37555cf76769e7876852869096a
Mun, Younghwan <younghwan.mun@teradata.com>
2020-06-10 22:30:53 -0400

    no message

---
+++ portfolio/static/assets/js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+    $('.close-sidebar-btn').click(function () {
+        var classToSwitch = $(this).attr('data-class');
+        var containerElement = '.app-container';
+        $(containerElement).toggleClass(classToSwitch);
+
+        var closeBtn = $(this);
+
+        if (closeBtn.hasClass('is-active')) {
+            closeBtn.removeClass('is-active');
+
+        } else {
+            closeBtn.addClass('is-active');
+        }
+    });
+
+    $('.mobile-toggle-nav').click(function () {
+        $(this).toggleClass('is-active');
+        $('.app-container').toggleClass('sidebar-mobile-open');
+    });
+
+    $('.mobile-toggle-header-nav').click(function () {
+        $(this).toggleClass('active');
+        $('.app-header__content').toggleClass('header-mobile-open');
+    });
+
+    var resizeClass = function () {
+        var win = document.body.clientWidth;
+        if (win < 1250) {
+            $('.app-container').addClass('closed-sidebar-mobile closed-sidebar');
+        } else {
+            $('.app-container').removeClass('closed-sidebar-mobile closed-sidebar');
+        }
+    };
+
+    $(window).on('resize', function () {
+        resizeClass();
+    });
+    resizeClass();
+});
