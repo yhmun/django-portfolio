b4e4c7026f9d0876adf3aa865a11b736690c3f56
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-03 20:41:04 -0500

    add templates

--- apps/example/adoptions/views.py
+++ apps/example/adoptions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "example/adoptions/index.html")
+
+def detail(request, pet_id):
+    return render(request, "example/adoptions/detail.html", {
+            'pet_id': pet_id
+        })
