e0a1f7bade18904653e3767d62b2a81c5a74d222
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-03 19:32:45 -0500

    change default database engine as postgres

--- web/settings.py
+++ web/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+                                             # "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", "web_dev"),
+                                             # os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
