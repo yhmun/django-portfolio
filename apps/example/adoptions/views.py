115919102af2b01250598c3c533ae18dc6aa352c
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-03 20:46:26 -0500

    read Pet model for views

--- apps/example/adoptions/views.py
+++ apps/example/adoptions/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Pet 
 
 def index(request):
-    return render(request, "example/adoptions/index.html")
+    pets = Pet.objects.all()
+    return render(request, "example/adoptions/index.html", {
+            'pets': pets
+        })
 
 def detail(request, pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('pet not found')
     return render(request, "example/adoptions/detail.html", {
-            'pet_id': pet_id
+            'pet': pet
         })
