f1b5809b63f48297dfc58469d0fd667d9e938875
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-04 13:57:33 -0500

    add jobs app

--- apps/example/jobs/urls.py
+++ apps/example/jobs/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "jobs"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:job_id>', views.detail, name='detail')
 ]
--- apps/example/jobs/views.py
+++ apps/example/jobs/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Job 
 
 def index(request):
-    return render(request, 'example/jobs/index.html')
+    jobs = Job.objects.all()
+    return render(request, 'example/jobs/index.html', {
+            'jobs': jobs
+        })
+
+def detail(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'example/jobs/detail.html', {
+            'job': job
+        })
