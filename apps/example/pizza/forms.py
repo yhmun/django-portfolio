3b99e1967cb5648026596f10f55fd03b46d7d1d9
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-05 11:10:07 -0500

    use submitted data

--- apps/example/pizza/forms.py
+++ apps/example/pizza/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class PizzaForm(forms.Form):
-    SIZE_CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    SIZE_CHOICES = [('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')]
     topping1 = forms.CharField(label='Topping 1', max_length=100)
     topping2 = forms.CharField(label='Topping 2', max_length=100)
     size = forms.ChoiceField(label='Size', choices=SIZE_CHOICES)
--- apps/example/pizza/views.py
+++ apps/example/pizza/views.py
@@ -5,5 +5,16 @@ def index(request):
     return render(request, 'example/pizza/index.html')
 
 def order(request):
-    form = PizzaForm()
-    return render(request, 'example/pizza/order.html', {'pizza_form': form})
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thanks for ordering! Your %s %s and %s pizza is on its way!' % (
+                filled_form.cleaned_data['size'], 
+                filled_form.cleaned_data['topping1'], 
+                filled_form.cleaned_data['topping2'],
+            )
+            new_form = PizzaForm()
+            return render(request, 'example/pizza/order.html', {'pizza_form': new_form, 'note': note}) 
+    else:  
+        form = PizzaForm()
+        return render(request, 'example/pizza/order.html', {'pizza_form': form})
