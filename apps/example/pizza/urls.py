3c2d64113a4f4f5005d36020c7ed64f618966c92
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-05 10:41:27 -0500

    add links

--- apps/example/pizza/urls.py
+++ apps/example/pizza/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'pizza'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('order', views.order, name='order'),
 ]
--- apps/example/pizza/views.py
+++ apps/example/pizza/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'example/pizza/index.html')
+
+def order(request):
+    return render(request, 'example/pizza/order.html')
