c4dc69a451f1baf21b6a59110c0927d830d43271
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-06 10:06:36 -0500

    add pagination

--- apps/example/store/api_views.py
+++ apps/example/store/api_views.py
@@ -1,13 +1,19 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from django.utils import timezone
 from .serializers import ProductSerializer
 from .models import Product 
 
+class ProductPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
+
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
     search_fields = ('name', 'description')
