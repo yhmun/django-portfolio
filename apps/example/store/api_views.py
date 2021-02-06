d7b9625d534ab7c93ca220d7bde53d98d517928c
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-06 11:02:23 -0500

    add RetriveUpdateDeletApiView

--- apps/example/store/api_views.py
+++ apps/example/store/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView #DestroyAPIView
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
@@ -44,9 +44,22 @@ class ProductCreate(CreateAPIView):
             raise  ValidationError({ 'price': 'A vaild number is required' })
         return super().create(request, *args, **kwargs)
 
-class ProductDestory(DestroyAPIView):
+# class ProductDestory(DestroyAPIView):
+#     queryset = Product.objects.all()
+#     lookup_field = 'id'
+
+#     def delete(self, request, *args, **kwargs):
+#         product_id = request.data.get('id')
+#         response = super().delete(request, *args, **kwargs)
+#         if response.status_code == 204:
+#             from django.core.cache import cache
+#             cache.delete('product_data_{}'.format(product_id))
+#         return response
+
+class ProductRetriveUpdateDestory(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     lookup_field = 'id'
+    serializer_class = ProductSerializer
 
     def delete(self, request, *args, **kwargs):
         product_id = request.data.get('id')
@@ -54,4 +67,17 @@ class ProductDestory(DestroyAPIView):
         if response.status_code == 204:
             from django.core.cache import cache
             cache.delete('product_data_{}'.format(product_id))
+        return response
+    
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            from django.core.cache import cache
+            product = response.data
+            cache.delete('product_data_{}'.format(product['id']), {
+                'name': product['name'],
+                'description': product['description'],
+                'price': product['price'],
+
+            })
         return response
