dbd5a31e52f1a1b90dde6f7d7de1297c27992474
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-06 11:09:59 -0500

    modify fields

--- apps/example/store/serializers.py
+++ apps/example/store/serializers.py
@@ -2,12 +2,19 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    is_on_sale = serializers.BooleanField(read_only=True)
+    current_price = serializers.FloatField(read_only=True)
+    description = serializers.CharField(min_length=2, max_length=200)
+
     class Meta:
         model = Product 
-        fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end')
+        fields = (
+            'id', 'name', 'description', 'price', 'sale_start', 'sale_end',
+            'is_on_sale', 'current_price',
+        )
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['is_on_sale'] = instance.is_on_sale()
-        data['current_price'] = instance.current_price()
-        return data
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     data['is_on_sale'] = instance.is_on_sale()
+    #     data['current_price'] = instance.current_price()
+    #     return data
